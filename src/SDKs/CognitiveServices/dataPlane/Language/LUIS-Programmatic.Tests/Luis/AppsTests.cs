906ef2e1ffc9d7b657b4bdf64eef30da177bac04
pablo costantini <pablo.costantini@southworks.com>
2017-11-27 16:20:58 -0800

    LUIS Programmatic SDK (v2.0) - Initial commit
    - Wrapper classes
    - UnitTest + SessionRecords

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/AppsTests.cs
@@ -0,0 +1,149 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using Xunit;
+
+    public class AppsTests: BaseTest
+    {
+        [Fact]
+        public void GetApplicationsList()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetApplicationsListAsync();
+
+                Assert.Equal(12, result.Count);
+                foreach (var app in result)
+                {
+                    Assert.True(Guid.TryParse(app.Id, out Guid id));
+                }
+            });
+        }
+
+        [Fact]
+        public void AddApplication()
+        {
+            UseClientFor(async client =>
+            {
+                var appId = await client.Apps.AddApplicationAsync(new ApplicationCreateObject
+                {
+                    Name = "New LUIS App",
+                    Description = "New LUIS App",
+                    Culture = "en-us",
+                    Domain = "Comics",
+                    UsageScenario = "IoT"
+                });
+
+                Assert.True(Guid.TryParse(appId, out Guid appGuid));
+                Assert.Equal(new Guid("9e6703ec-56fe-48ce-8a72-10d592f6056d"), appGuid);
+            });
+        }
+
+        [Fact]
+        public void GetApplicationInfo()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetApplicationInfoAsync( appId);
+                Assert.Equal(appId, result.Id);
+            });
+        }
+
+        [Fact]
+        public void RenameApplication()
+        {
+            UseClientFor(async client =>
+            {
+                await client.Apps.RenameApplicationAsync(appId, new ApplicationUpdateObject
+                {
+                    Name = "LUIS App name updated",
+                    Description = "LUIS App description updated"
+                });
+
+                var app = await client.Apps.GetApplicationInfoAsync(appId);
+
+                Assert.Equal("LUIS App name updated", app.Name);
+                Assert.Equal("LUIS App description updated", app.Description);
+            });
+        }
+
+        [Fact]
+        public void GetApplicationDomains()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetApplicationDomainsAsync();
+                foreach (var domain in result)
+                {
+                    Assert.False(string.IsNullOrWhiteSpace(domain));
+                }
+            });
+        }
+
+        [Fact]
+        public void GetApplicationCultures()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetApplicationCulturesAsync();
+                foreach (var culture in result)
+                {
+                    var cult = new CultureInfo(culture.Code);
+                    Assert.Equal(cult.Name.ToLowerInvariant(), culture.Code);
+                }
+            });
+        }
+
+        [Fact]
+        public void GetApplicationUsageScenarios()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetApplicationUsageScenariosAsync();
+                foreach (var scenario in result)
+                {
+                    Assert.False(string.IsNullOrWhiteSpace(scenario));
+                }
+            });
+        }
+
+        [Fact]
+        public void GetAvailableCustomPrebuiltDomains()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Apps.GetAvailableCustomPrebuiltDomainsAsync();
+                foreach (var prebuiltDomain in result)
+                {
+                    Assert.NotNull(prebuiltDomain);
+                    Assert.False(string.IsNullOrWhiteSpace(prebuiltDomain.Description));
+                    Assert.NotNull(prebuiltDomain.Intents);
+                    Assert.NotNull(prebuiltDomain.Entities);
+                }
+            });
+        }
+
+        [Fact]
+        public void GetAvailableCustomPrebuiltDomainsForCulture()
+        {
+            UseClientFor(async client =>
+            {
+                var resultsUS = await client.Apps.GetAvailableCustomPrebuiltDomainsForCultureAsync("en-US");
+                var resultsCN = await client.Apps.GetAvailableCustomPrebuiltDomainsForCultureAsync("zh-CN");
+
+                foreach (var resultUS in resultsUS)
+                {
+                    Assert.DoesNotContain(resultsCN, r => r.Description == resultUS.Description);
+                }
+                foreach (var resultCN in resultsCN)
+                {
+                    Assert.DoesNotContain(resultsUS, r => r.Description == resultCN.Description);
+                }
+            });
+        }
+    }
+}
---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/SubscriptionsTests.cs
@@ -0,0 +1,63 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using System;
+    using System.Linq;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using Xunit;
+
+    public class SubscriptionsTests : BaseTest
+    {
+        [Fact]
+        public void GetSubscriptionsList()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.User.GetUserSubscriptionKeysAsync();
+
+                Assert.Equal(2, result.Count);
+                foreach (var subscriptionKeyInfo in result)
+                {
+                    Assert.True(Guid.TryParse(subscriptionKeyInfo.SubscriptionKey, out Guid id));
+                }
+            });
+        }
+
+        [Fact]
+        public void RenameSubscription()
+        {
+            var subscriptionName = $"LUIS Subscription name updated ({Guid.NewGuid().ToString()})";
+            UseClientFor(async client =>
+            {
+                await client.User.RenameSubscriptionKeyAsync(new UserSubscriptionCreateObject
+                {
+                    SubscriptionKey = BaseTest.subscriptionKey,
+                    SubscriptionName = subscriptionName,
+                });
+
+                var subscriptionList = await client.User.GetUserSubscriptionKeysAsync();
+
+                Assert.Contains(subscriptionList, s => s.SubscriptionKey == BaseTest.subscriptionKey);
+                var subscription = subscriptionList.Single(s => s.SubscriptionKey == BaseTest.subscriptionKey);
+                Assert.Equal(subscriptionName, subscription.SubscriptionName);
+            });
+        }
+
+        [Fact]
+        public void AddSubscription()
+        {
+            var subscriptionName = $"LUIS Subscription name ({Guid.NewGuid().ToString()})";
+            UseClientFor(async client =>
+            {
+                await client.User.AddSubscriptionKeyAsync(new UserSubscriptionCreateObject
+                {
+                    SubscriptionName = subscriptionName
+                });
+
+                var subscriptionList = await client.User.GetUserSubscriptionKeysAsync();
+
+                Assert.Contains(subscriptionList, s => s.SubscriptionName == subscriptionName);
+            });
+        }
+    }
+}
