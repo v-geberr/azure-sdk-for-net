724feb6f362ef3ef294163fa6bae3d7a1f834cd0
Enzo Cano <enzo.cano@southworks.com>
2017-11-28 12:01:46 -0300

    Create test for Permissions_GetApplicationUserAccessList op

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/PermissionsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Xunit;
+
+    public class PermissionsTests: BaseTest
+    {
+        [Fact]
+        public void GetApplicationsList()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Permissions.GetApplicationUserAccessListAsync(appId);
+
+                Assert.Equal("owner.user@microsoft.com", result.Owner);
+                Assert.Equal(new string[] { "guest@outlook.com" }, result.Emails);
+            });
+        }
+    }
+}
