912b8616c87584a670f4bbcdc0a9c58bd3e4f82c
JuanAr <juan.arguello@southworks.com>
2017-11-29 17:44:58 -0300

    Add Composite Model tests

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LUIS.Programmatic.Tests.Luis;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+using Xunit;
+
+namespace Microsoft.Azure.CognitiveServices.LUIS.Programmatic.Tests.Luis
+{
+    public class ModelsTests : BaseTest
+    {
+        [Fact]
+        public void GetApplicationVersionCompositeEntityInfos()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Model.GetApplicationVersionCompositeEntityInfosAsync(BaseTest.appId, "0.1");
+
+                Assert.Equal(3, result.Count);
+                foreach (var entity in result)
+                {
+                    Assert.True(Guid.TryParse(entity.Id, out Guid id));
+                }
+            });
+        }
+
+        [Fact]
+        public void CreateCompositeEntityExtractor()
+        {
+            UseClientFor(async client =>
+            {
+                var entityId = Guid.NewGuid().ToString().Replace("-", string.Empty);
+                var childEntity = new ChildEntity(entityId, "datetime");
+                var hierarchicalModel = new HierarchicalModelCreateObject(new List<string>() { "datetime" }, name: "Reservation");
+                var result = await client.Model.CreateCompositeEntityExtractorAsync(BaseTest.appId, "0.1", hierarchicalModel);
+
+                Assert.True(Guid.TryParse(result, out Guid id));
+            });
+        }
+
+        [Fact]
+        public void GetCompositeEntityInfo()
+        {
+            UseClientFor(async client =>
+            {
+                var entities = await client.Model.GetApplicationVersionCompositeEntityInfosAsync(BaseTest.appId, "0.1");
+
+                var result = await client.Model.GetCompositeEntityInfoAsync(BaseTest.appId, "0.1", entities.First().Id);
+
+                Assert.True(Guid.TryParse(result.Id, out Guid id));
+            });
+        }
+
+    }
+}
