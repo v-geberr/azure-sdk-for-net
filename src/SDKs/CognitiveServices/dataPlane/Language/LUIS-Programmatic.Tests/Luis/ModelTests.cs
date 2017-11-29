7b971876e2c77b325c3d9877e29b3a1e204c707f
Enzo Cano <enzo.cano@southworks.com>
2017-11-29 17:35:27 -0300

    Add test for Model_DeleteCustomPrebuiltDomainModels op

--- src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelTests.cs
@@ -30,6 +30,30 @@
                 }
             });
         }
-        
+
+        [Fact]
+        public void DeleteCustomPrebuiltDomainModels()
+        {
+            UseClientFor(async client =>
+            {
+                var versionsApp = await client.Versions.GetApplicationVersionsAsync(appId);
+                var version = versionsApp.FirstOrDefault().Version;
+                var prebuiltDomain = new PrebuiltDomainCreateBaseObject
+                {
+                    DomainName = "Gaming"
+                };
+
+                var results = await client.Model.AddCustomPrebuiltDomainToApplicationAsync(appId, version, prebuiltDomain);
+                var prebuiltModels = await client.Model.GetCustomPrebuiltDomainModelsInfoAsync(appId, version);
+
+                Assert.True(prebuiltModels.Count > 0);
+
+                await client.Model.DeleteCustomPrebuiltDomainModelsAsync(appId, version, prebuiltDomain.DomainName);
+
+                prebuiltModels = await client.Model.GetCustomPrebuiltDomainModelsInfoAsync(appId, version);
+
+                Assert.True(prebuiltModels.Count == 0);
+            });
+        }
     }
 }
