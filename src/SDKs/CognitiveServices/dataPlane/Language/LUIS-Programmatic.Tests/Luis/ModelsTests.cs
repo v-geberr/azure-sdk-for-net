5b939786affa73d3d50a4e3fd770340b20f0b7b9
JuanAr <juan.arguello@southworks.com>
2017-11-29 17:50:55 -0300

    Add test for Model_GetApplicationVersionCompositeEntityInfos

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LUIS.Programmatic.Tests.Luis;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+using Xunit;
+
+namespace Microsoft.Azure.CognitiveServices.LUIS.Programmatic.Tests.Luis
+{
+    public class ModelsTests : BaseTest
+    {
+        [Fact]
+        public void GetApplicationVersionCompositeEntityInfos()
+        {
+            UseClientFor(async client =>
+            {
+                var result = await client.Model.GetApplicationVersionCompositeEntityInfosAsync(BaseTest.appId, "0.1");
+
+                Assert.Equal(3, result.Count);
+                foreach (var entity in result)
+                {
+                    Assert.True(Guid.TryParse(entity.Id, out Guid id));
+                }
+            });
+        }
+    }
+}
