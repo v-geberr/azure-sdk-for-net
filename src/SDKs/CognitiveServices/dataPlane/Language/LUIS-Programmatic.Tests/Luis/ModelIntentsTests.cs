d9063942131a4f815f90b167dda576cd7da98bf2
Enzo Cano <enzo.cano@southworks.com>
2017-11-30 16:30:41 -0300

    Add test for Model_GetApplicationVersionIntentInfos op

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelIntentsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using System.Linq;
+    using Xunit;
+
+    public class ModelIntentsTests : BaseTest
+    {
+        [Fact]
+        public void GetApplicationVersionIntentInfos()
+        {
+            UseClientFor(async client =>
+            {
+                var version = "0.1";
+                var intents = await client.Model.GetApplicationVersionIntentInfosAsync(appId, version);
+
+                Assert.True(intents.All(i => i.ReadableType.Equals("Intent Classifier")));
+            });
+        }
+    }
+}
