de275d02f8294beb1d705338ce5e5661b1cd56bb
Enzo Cano <enzo.cano@southworks.com>
2017-11-30 17:50:06 -0300

    Add test for Model_GetAvailablePrebuiltEntityExtractors op

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelprebuiltsTests.cs
@@ -0,0 +1,23 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    public class ModelPrebuiltsTests : BaseTest
+    {
+        [Fact]
+        public void GetAvailablePrebuiltEntityExtractorsAsync()
+        {
+            UseClientFor(async client =>
+            {
+                var version = "0.1";
+                var prebuiltEntities = await client.Model.GetAvailablePrebuiltEntityExtractorsAsync(appId, version);
+
+                Assert.True(prebuiltEntities.Count > 0);
+            });
+        }
+    }
+}
