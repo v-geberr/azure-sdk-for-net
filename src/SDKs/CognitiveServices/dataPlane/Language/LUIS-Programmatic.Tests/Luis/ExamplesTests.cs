52291a125799ec4c5105d1a1c386a5d14878bdc4
pablo costantini <pablo.costantini@southworks.com>
2017-11-30 10:06:07 -0800

    Examples_ReviewLabeledExamples Tests

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ExamplesTests.cs
@@ -0,0 +1,45 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using System.Linq;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using Xunit;
+
+    public class ExamplesTests : BaseTest
+    {
+        private const string versionId = "0.1";
+        
+        [Fact]
+        public void GetExamples()
+        {
+            UseClientFor(async client =>
+            {
+                var examples = await client.Examples.ReviewLabeledExamplesAsync(appId, versionId);
+
+                Assert.NotEmpty(examples);
+            });
+        }
+
+        [Fact]
+        public void GetExamples_ForEmptyApplication_ReturnsEmpty()
+        {
+            UseClientFor(async client =>
+            {
+                var appId = await client.Apps.AddApplicationAsync(new ApplicationCreateObject
+                {
+                    Name = "Examples Test App",
+                    Description = "New LUIS App",
+                    Culture = "en-us",
+                    Domain = "Comics",
+                    UsageScenario = "IoT"
+                });
+
+                var examples = await client.Examples.ReviewLabeledExamplesAsync(appId, versionId);
+
+                await client.Apps.DeleteApplicationAsync(appId);
+
+                Assert.Empty(examples);
+            });
+        }
+    }
+}
