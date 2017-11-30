c88d1f51caa022aef03274d38f6b861436ce06d0
pablo costantini <pablo.costantini@southworks.com>
2017-11-30 06:04:07 -0800

    Model_GetApplicationVersionEntityInfos & Model_CreateEntityExtractor Tests

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/ModelEntitiesTests.cs
@@ -0,0 +1,52 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using Xunit;
+
+    public class ModelSimpleEntitiesTests : BaseTest
+    {
+        private const string versionId = "0.1";
+
+        [Fact]
+        public void GetEntities()
+        {
+            UseClientFor(async client =>
+            {
+                var entityId = await client.Model.CreateEntityExtractorAsync(appId, versionId, new ModelCreateObject
+                {
+                    Name = "Existing Entity Test"
+                });
+
+                var results = await client.Model.GetApplicationVersionEntityInfosAsync(appId, versionId);
+
+                Assert.NotEqual(0, results.Count);
+                Assert.Contains(results, o => o.Name == "Existing Entity Test");
+
+                await client.Model.DeleteEntityModelAsync(appId, versionId, entityId);
+            });
+        }
+
+        [Fact]
+        public void CreateEntity()
+        {
+            UseClientFor(async client =>
+            {
+                var entityId = await client.Model.CreateEntityExtractorAsync(appId, versionId, new ModelCreateObject
+                {
+                    Name = "New Entity Test"
+                });
+
+                var result = await client.Model.GetEntityInfoAsync(appId, versionId, entityId);
+
+                Assert.NotNull(result);
+                Assert.Equal("New Entity Test", result.Name);
+                Assert.Equal("Entity Extractor", result.ReadableType);
+
+                await client.Model.DeleteEntityModelAsync(appId, versionId, entityId);
+            });
+        }
+    }
+}
