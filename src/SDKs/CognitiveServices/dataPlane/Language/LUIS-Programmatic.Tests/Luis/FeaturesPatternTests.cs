b04065e7ecf2fe20f767949176739ff3d9213634
Enzo Cano <enzo.cano@southworks.com>
2017-11-30 19:12:38 -0300

    Add test for Features_GetApplicationVersionPatternFeatures op

---
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS-Programmatic.Tests/Luis/FeaturesPatternTests.cs
@@ -0,0 +1,23 @@
+﻿namespace LUIS.Programmatic.Tests.Luis
+{
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic.Models;
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    public class FeaturesPatternTests : BaseTest
+    {
+        [Fact]
+        public void GetApplicationVersionPatternFeaturesAsync()
+        {
+            UseClientFor(async client =>
+            {
+                var version = "0.1";
+                var patterns = await client.Features.GetApplicationVersionPatternFeaturesAsync(appId, version);
+
+                Assert.True(patterns.Count > 0);
+            });
+        }
+    }
+}
